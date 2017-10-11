5851df5dee04a04a5d86f92197a2a157a7465168
krisbb <gajek.krzysiek@gmail.com>
2017-10-11 22:54:59 +0200

    Initial description

---
+++ lab1/lab1.py
@@ -0,0 +1,9 @@
+
+"""
+Python version 3.6.2
+This program allow to encode/decode files from/to base64
+usage:
+Encode with -e, Decode -d
+python lab1.py inputFile -e outputFile
+Implementation based on RFC 2045
+"""
